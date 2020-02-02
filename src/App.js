b1c6abf5bcca1f6819752441ccf5ebf2b9504261
skliu <skliu@ctrip.com>
2020-02-02 13:57:33 +0800

    feat: init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div id="app">
+        hello react
+      </div>
+    );
+  }
+}
+
+export default App;
